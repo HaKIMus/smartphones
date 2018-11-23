39a10b9fa5156b8da02cadea179e10af12cb7ea4
HaKIMus <webdevhakim@gmail.com>
2018-11-24 00:26:14 +0100

    Init x2

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # smartphones
 Smartphones + REST API + Symfony 4 + PHPUnit + Docker in action
+
+To set up application exec the following commands:
+
+    composer update
+
+    docker-compose build
+    docker-compose up -d
+
+The website is running under **8080** port.
+
+If you don't have docker yet:
+https://docs.docker.com/install/
+
+**Tests**
+
+To run tests execute following command:
+
+    php bin/phpspec run
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hakim
- * Date: 23.11.18
- * Time: 22:15
- */
 
-class HomeController
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends Controller
 {
-
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        return new Response('Hello world!');
+        // TODO implement the index method
+    }
 }
