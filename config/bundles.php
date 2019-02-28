3223dd5227e8942f07620d991be142872abf915f
HaKIMus <webdevhakim@gmail.com>
2019-02-28 10:40:12 +0100

    Feature: Required profiler package

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
